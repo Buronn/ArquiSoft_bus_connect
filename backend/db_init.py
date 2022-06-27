23cf035389c5e8907b23e118b87b08b6746d604c
Buronn <fernando.buron@mail.udp.cl>
2022-06-27 13:32:06 -0400

    init

--- backend/db_init.py
+++ backend/db_init.py
@@ -4,13 +4,14 @@ from services.config import Config
 from services.models import Usuario, Base
 from sqlalchemy.orm import Session
 import datetime
+import os
 
 def init_db():
     engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    if not session.query(Usuario).filter(Usuario.email == 'admin').first():
-        usuario = Usuario(username='admin', password='admin', email='admin', phone='phone', fecha_ingreso=datetime.datetime.now())
+    if not session.query(Usuario).filter(Usuario.email == os.environ["ADMIN_ACCOUNT"]).first():
+        usuario = Usuario(username='admin', password=os.environ["ADMIN_PASSWORD"], email=os.environ["ADMIN_ACCOUNT"], phone='000', fecha_ingreso=datetime.datetime.now())
         session.add(usuario)
     session.commit()
     session.close()
