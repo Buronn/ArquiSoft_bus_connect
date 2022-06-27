81e93daa0e6fac119cd3b3494fb6078b5cff2123
Buronn <fernando.buron@mail.udp.cl>
2022-06-27 15:54:42 -0400

    rename

---
+++ backend/cliregister.py
@@ -0,0 +1,24 @@
+from clients.Client import Client
+from getpass import getpass
+        
+if __name__ == "__main__":
+    print("Service: Registro")
+    keep_alive = True
+    try:
+        while(keep_alive):
+            user = input("Ingrese usuario: ")
+            password = getpass("Ingrese contraseña: ")
+            email = input("Ingrese email: ")
+            phone = input("Ingrese telefono: ")
+
+            try: 
+                a = Client("bregi")
+                climsg = user + " " + password + " " + email + " " + phone
+                msg = a.exec_client(debug=True, climsg=climsg)
+                print("###################################\n\n", msg, "\n\n###################################")
+            except Exception as e:
+                print("Error: ", e)
+    except KeyboardInterrupt:
+        print("\nCerrando cliente, hasta pronto ....")
+        keep_alive = False
+        exit()
