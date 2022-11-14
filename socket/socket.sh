4066aba6ed9be8e154c084194f911bf895c464d8
FernandoBuron <fernando.buron@mail.udp.cl>
2022-11-14 19:35:50 -0300

    ssh port change from 22 to 34567

--- socket/socket.sh
+++ socket/socket.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -e ssh -tt -L *:5000:localhost:5000 -o StrictHostKeyChecking=no ${USER}@${SERVER} 'date; /bin/bash'
+sshpass -e ssh -p 34567 -tt -L *:5000:localhost:5000 -o StrictHostKeyChecking=no ${USER}@${SERVER} 'date; /bin/bash'
